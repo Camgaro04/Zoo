70bf71af01573e0b2a52b3554f89e2e3d9ef2f1b
AndresCorrea97 <47647246+AndresCorrea97@users.noreply.github.com>
2019-03-10 17:56:19 -0500

    1
    Se llenaron las clases entitys  Horario,HorarioTipoAlimentos,TipoAlimentos

--- src/main/java/co/edu/konrad/zoo/entities/HorarioTipoAlimentosEntity.java
+++ src/main/java/co/edu/konrad/zoo/entities/HorarioTipoAlimentosEntity.java
@@ -5,10 +5,68 @@
  */
 package co.edu.konrad.zoo.entities;
 
+
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
 /**
  *
- * @author windows
+ * @author Andres Correa
  */
-public class HorarioTipoAlimentosEntity {
+@Entity
+public class HorarioTipoAlimentosEntity implements Serializable {
     
+    
+      /**
+     * 
+     */
+    @JoinColumn(name = "id_Horario")
+    @ManyToOne
+    private HorarioEntity idHorario;
+
+    /**
+     * 
+     */ 
+   
+    @JoinColumn(name = "id_tipo_Alimentos")
+    @ManyToOne
+    private TipoAlimentoEntity idTipoAlimentos;
+   
+    public HorarioTipoAlimentosEntity() {
+    }
+
+    public HorarioTipoAlimentosEntity(HorarioEntity idHorario, TipoAlimentoEntity idTipoAlimentos) {
+        this.idHorario = idHorario;
+        this.idTipoAlimentos = idTipoAlimentos;
+    }
+
+    public void setIdHorario(HorarioEntity idHorario) {
+        this.idHorario = idHorario;
+    }
+
+    public void setIdTipoAlimentos(TipoAlimentoEntity idTipoAlimentos) {
+        this.idTipoAlimentos = idTipoAlimentos;
+    }
+
+    public HorarioEntity getIdHorario() {
+        return idHorario;
+    }
+
+    public TipoAlimentoEntity getIdTipoAlimentos() {
+        return idTipoAlimentos;
+    }
+
+   
+    
+  
+  
 }
