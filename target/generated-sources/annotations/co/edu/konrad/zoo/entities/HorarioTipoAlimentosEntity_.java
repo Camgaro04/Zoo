d22108ab2375d44db0a3f9372a3f9c9596c9b1df
Luis Suarez <luisc.suareza@konradlorenz.edu.co>
2019-03-11 15:16:10 -0500

    Cosas a ignorar
    Lo que debe ignorar el repo

--- target/generated-sources/annotations/co/edu/konrad/zoo/entities/HorarioTipoAlimentosEntity_.java
+++ target/generated-sources/annotations/co/edu/konrad/zoo/entities/HorarioTipoAlimentosEntity_.java
@@ -1,15 +1,17 @@
 package co.edu.konrad.zoo.entities;
 
 import co.edu.konrad.zoo.entities.HorarioEntity;
+import co.edu.konrad.zoo.entities.HorarioTipoAlimentosEntity.PK_A;
 import co.edu.konrad.zoo.entities.TipoAlimentoEntity;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-10T19:43:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-11T14:25:22")
 @StaticMetamodel(HorarioTipoAlimentosEntity.class)
 public class HorarioTipoAlimentosEntity_ { 
 
+    public static volatile SingularAttribute<HorarioTipoAlimentosEntity, PK_A> clave;
     public static volatile SingularAttribute<HorarioTipoAlimentosEntity, HorarioEntity> idHorario;
     public static volatile SingularAttribute<HorarioTipoAlimentosEntity, TipoAlimentoEntity> idTipoAlimentos;
 
