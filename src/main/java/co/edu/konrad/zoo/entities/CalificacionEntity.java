cf038b3b4cb75a8f6dcd6fa946c1813cba0164c3
Luis Suarez <luisc.suareza@konradlorenz.edu.co>
2019-03-14 14:00:59 -0500

    entity de calificaciones
    entity de calificaciones

--- src/main/java/co/edu/konrad/zoo/entities/CalificacionEntity.java
+++ src/main/java/co/edu/konrad/zoo/entities/CalificacionEntity.java
@@ -1,9 +1,48 @@
 package co.edu.konrad.zoo.entities;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  *
  * @author Grupo Zoo
  */
 public class CalificacionEntity {
+     /**
+     * 
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long idCalificacion;
+   
+     /**
+     *
+     */
+    @JoinColumn(name = "id_show")
+    @ManyToOne
+    private ShowEntity idShow;
     
+    /**
+     *
+     */
+    @JoinColumn(name = "id_animal")
+    @ManyToOne
+    private AnimalesEntity idAnimal;
+
+     /**
+     * 
+     */
+    @Column(name = "calificacion")
+    private int calificacion;
+    
+     /**
+     * 
+     */
+    @Column(name = "onservacion")
+    private String observacion;
+   
 }
