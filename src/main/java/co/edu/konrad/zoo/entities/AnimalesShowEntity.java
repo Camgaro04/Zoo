690b26c01576200805fb4412f2287a8c3a31f6fd
Luis Suarez <luisc.suareza@konradlorenz.edu.co>
2019-03-14 13:55:50 -0500

    Entitys de animales

--- src/main/java/co/edu/konrad/zoo/entities/AnimalesShowEntity.java
+++ src/main/java/co/edu/konrad/zoo/entities/AnimalesShowEntity.java
@@ -1,9 +1,42 @@
 package co.edu.konrad.zoo.entities;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  *
- * @author Grupo Zoo
+ * @author Luis
  */
 public class AnimalesShowEntity {
     
+    @EmbeddedId
+    private PK_AnimalesShow clave;
+
+    /**
+     *
+     */
+    @JoinColumn(name = "id_animal")
+    @ManyToOne
+    private AnimalesEntity idAnimal;
+
+    /**
+     *
+     */
+    @JoinColumn(name = "id_show")
+    @ManyToOne
+    private ShowEntity idShow;
+    
+    /**
+     * 
+     */
+     public class PK_AnimalesShow {
+
+        @Column
+        private String idAnimal;
+        
+        @Column
+        private String idShow;
+    }
 }
