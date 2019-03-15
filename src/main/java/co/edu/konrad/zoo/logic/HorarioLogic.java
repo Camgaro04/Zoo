ac0f01c2f128e00547635929a81cf5b420a54a45
AndresCorrea97 <47647246+AndresCorrea97@users.noreply.github.com>
2019-03-14 23:52:16 -0500

    HorarioLogic

--- src/main/java/co/edu/konrad/zoo/logic/HorarioLogic.java
+++ src/main/java/co/edu/konrad/zoo/logic/HorarioLogic.java
@@ -4,6 +4,38 @@ package co.edu.konrad.zoo.logic;
  *
  * @author Grupo Zoo
  */
+import co.edu.konrad.zoo.entities.HorarioEntity;
+import co.edu.konrad.zoo.persistence.HorarioPersistence;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+@Stateless
 public class HorarioLogic {
+     @Inject
+     private HorarioPersistence persistence;
+    
+      public List<HorarioEntity> findAll(){
+        return persistence.findAll();
+    }
+    
+    public HorarioEntity findById(long id){
+        return persistence.find(id);
+    }
+    
+    
+     public HorarioEntity insert(HorarioEntity p){
+        persistence.create(p);
+        return p;
+    }
+     
+    public HorarioEntity update(HorarioEntity p){
+        persistence.merge(p);
+        return p;
+    }
+    
+    
+    public void delete(int id){
+        persistence.delete(id);
+    }
     
 }
